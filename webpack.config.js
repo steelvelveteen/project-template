414afc90038751000772c2736e088c445198ae40
steelvelveteen <joeyvico@gmail.com>
2020-04-30 12:09:43 +0930

    Completed boilerplate adding typescript and tsc -init config file

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = ({ mode }) => {
+    return {
+        mode,
+        entry: {
+            main: './src/index.ts'
+        },
+        devtool: 'inline-source-map',
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: '[name].[contentHash].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/i,
+                    use: 'html-loader'
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.scss$/i,
+                    use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+                }
+
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/template.html',
+                filename: 'index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'main.css'
+            })
+        ]
+    }
+}
