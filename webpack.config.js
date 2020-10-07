f445467336833f7abc313c9ed1c2193f70a23d95
steelvelveteen <joeyvico@gmail.com>
2020-10-08 07:11:47 +1030

    Updated webpack to output build to dist instead of public as in tsconfig file

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = ({ mode }) => {
         },
         devtool: 'inline-source-map',
         output: {
-            path: path.resolve(__dirname, 'public'),
+            path: path.resolve(__dirname, 'dist'),
             filename: '[name].[contentHash].js'
         },
         module: {
