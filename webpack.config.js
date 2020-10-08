61e390c52814aea705ec5dbff40d705048b9b80f
steelvelveteen <joeyvico@gmail.com>
2020-10-08 19:50:35 +1030

    Added file loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,14 @@ module.exports = ({ mode }) => {
                 {
                     test: /\.scss$/i,
                     use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+                },
+                {
+                  test: /\.(png|jpe?g|gif)$/i,
+                  use: [
+                    {
+                      loader: "file-loader"
+                    }
+                  ]
                 }
 
             ]
